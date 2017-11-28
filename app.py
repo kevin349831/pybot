28b02becc35fe9c3bf06031d238fc808674f4409
haoyu <kevin349831@gmail.com>
2017-11-28 21:32:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -673,7 +673,57 @@ def choiceMessage(message):
     else:
         return('不會使用嗎？')
     
-
+def This_is_test(stockName):
+    stockName = 'GOOG'
+    carousel_template_message = TemplateSendMessage(
+        alt_text='Carousel template',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://example.com/item1.jpg',
+                    title='this is menu1',
+                    text='description1',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='postback1',
+                            text='postback text1',
+                            data='action=buy&itemid=1'
+                        ),
+                        MessageTemplateAction(
+                            label='message1',
+                            text='message text1'
+                        ),
+                        URITemplateAction(
+                            label='uri1',
+                            uri='http://example.com/1'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://example.com/item2.jpg',
+                    title='this is menu2',
+                    text='description2',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='postback2',
+                            text='postback text2',
+                            data='action=buy&itemid=2'
+                        ),
+                        MessageTemplateAction(
+                            label='message2',
+                            text='message text2'
+                        ),
+                        URITemplateAction(
+                            label='uri2',
+                            uri='http://example.com/2'
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    return(carousel_template_message)
+    
 #this part is my code
 
 @app.route("/callback", methods=['POST'])
@@ -697,9 +747,7 @@ def callback():
 def handle_message(event):
     temp_text = event.message.text
     if temp_text == "news":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text = "this is test.")) #event.message.text
+        This_is_test(temp_text)
     else:
         line_bot_api.reply_message(
             event.reply_token,
